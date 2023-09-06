685b5788d7f26ae78aeed924222e19c4420a0176
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-09-07 02:22:17 +0530

    initial commit

---
+++ server.mjs
@@ -0,0 +1,59 @@
+import http from "http";
+import fs from "fs";
+
+const PORT = 3000;
+
+const contentTypes = {
+    html: "text/html",
+    plain: "text/plain",
+};
+
+const sendErrorResponse = (res) => {
+    const filePath = "./views/routes/404.html";
+    sendFileResponse(res, filePath, contentTypes.html, 404);
+};
+
+const sendFileResponse = (res, filePath, contentType, statusCode = 200) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) {
+            if (statusCode === 404) {
+                res.statusCode = 404;
+                res.setHeader("Content-Type", "text/plain");
+                res.end("Not Found");
+            } else {
+                sendErrorResponse(res);
+            }
+        } else {
+            res.statusCode = statusCode;
+            res.setHeader("Content-Type", contentType);
+            res.end(data);
+        }
+    });
+};
+
+const server = http.createServer((req, res) => {
+    const reqURL = req.url;
+
+    switch (reqURL) {
+        case "/":
+            sendFileResponse(res, "./views/index.html", "text/html");
+            break;
+
+        case "/about":
+            sendFileResponse(res, "./views/routes/about.html", "text/html");
+            break;
+        case "/contact-me":
+            sendFileResponse(
+                res,
+                "./views/routes/contact-me.html",
+                "text/html"
+            );
+            break;
+        default:
+            sendErrorResponse(res);
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`listening to port ${PORT}`);
+});
